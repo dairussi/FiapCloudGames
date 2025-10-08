7e43e7cda204917c8164636830f12109692f86c2
anabordinhon <anabordinhon@outlook.com.br>
2025-10-07 21:15:37 -0300

    feat: ajuste da controller

--- API/Controllers/Games/GamesController.cs
+++ API/Controllers/Games/GamesController.cs
@@ -1,14 +1,36 @@
-
-using FiapCloudGames.Application.Common;
 using FiapCloudGames.Application.Games.UseCases.Commands.AddGame;
+using FiapCloudGames.Application.Games.UseCases.Queries.GetGameById;
+using FiapCloudGames.Application.Games.UseCases.Queries.GetGamesPaged;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FiapCloudGames.API.Controllers.Games;
 
 [ApiController]
-[Route("api/games")]
+[Route("api/[controller]")]
 public class GamesController : ControllerBase
 {
+
+    [HttpGet("{publicId")]
+    public async Task<IActionResult> GetGameById([FromRoute] Guid publicId, [FromServices] IGetGameByIdQueryHandler handler, CancellationToken cancellationToken)
+    {
+        var query = new GetGameByIdQuery(publicId);
+        var result = await handler.Handle(query, cancellationToken);
+        return result.ToOkActionResult();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetGamesPaged(
+        [FromServices] IGetGamesPagedQueryHandler handler,
+        CancellationToken cancellationToken,
+        [FromQuery] int page = 1,
+        [FromQuery] int pageSize = 10)
+    {
+        var query = new GetGamesPagedQuery(page, pageSize);
+        var result = await handler.Handle(query, cancellationToken);
+
+        return result.ToOkActionResult();
+    }
+
 
     [HttpPost]
     public async Task<IActionResult> AddOrUpdatGame(
@@ -18,6 +40,6 @@ public class GamesController : ControllerBase
     {
         var command = input.MapToCommand();
         var result = await handler.Handle(command, cancellationToken);
-        return result.ToCreatedActionResult("/");
+        return result.ToCreatedActionResult($"/api/games/{result.Data.PublicId}");
     }
 }
--- Application/Games/UseCases/Queries/GetGameById/IGetGameByIdQueryHandler.cs
+++ Application/Games/UseCases/Queries/GetGameById/IGetGameByIdQueryHandler.cs
@@ -1,5 +1,9 @@
-﻿namespace FiapCloudGames.Application.Games.UseCases.Queries.GetGameById;
+using FiapCloudGames.Application.Common;
+using FiapCloudGames.Domain.Games.Entities;
+
+namespace FiapCloudGames.Application.Games.UseCases.Queries.GetGameById;
 
 public interface IGetGameByIdQueryHandler
 {
+    Task<ResultData<Game>> Handle(GetGameByIdQuery query, CancellationToken cancellationToken);
 }
