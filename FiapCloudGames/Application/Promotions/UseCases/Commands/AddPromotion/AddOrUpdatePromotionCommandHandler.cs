39a0b097b78467c870ba98b5f139f0a4307707c3
Daiana Russi <daianarussi.agroconsult@gmail.com>
2025-10-28 21:13:27 -0300

    fix

--- FiapCloudGames/Application/Promotions/UseCases/Commands/AddPromotion/AddOrUpdatePromotionCommandHandler.cs
+++ FiapCloudGames/Application/Promotions/UseCases/Commands/AddPromotion/AddOrUpdatePromotionCommandHandler.cs
@@ -3,16 +3,26 @@ using FiapCloudGames.Application.Promotions.Mappers;
 using FiapCloudGames.Application.Promotions.Outputs;
 using FiapCloudGames.Domain.Promotions.Entities;
 using FiapCloudGames.Domain.Promotions.Ports;
+using FiapCloudGames.Domain.Games.Ports;
+using FiapCloudGames.Domain.Users.Ports;
+using System.Linq;
 
 namespace FiapCloudGames.Application.Promotions.UseCases.Commands.AddPromotion;
 
 public class AddOrUpdatePromotionCommandHandler : IAddOrUpdatePromotionCommandHandler
 {
     private readonly IPromotionCommandRepository _promotionCommandRepository;
+    private readonly IGameQueryRepository _gameQueryRepository;
+    private readonly IUserQueryRepository _userQueryRepository;
 
-    public AddOrUpdatePromotionCommandHandler(IPromotionCommandRepository promotionCommandRepository)
+    public AddOrUpdatePromotionCommandHandler(
+        IPromotionCommandRepository promotionCommandRepository,
+        IGameQueryRepository gameQueryRepository,
+        IUserQueryRepository userQueryRepository)
     {
         _promotionCommandRepository = promotionCommandRepository;
+        _gameQueryRepository = gameQueryRepository;
+        _userQueryRepository = userQueryRepository;
     }
 
     public async Task<ResultData<PromotionOutput>> Handle(AddOrUpdatePromotionCommand command, CancellationToken cancellationToken)
@@ -22,6 +32,25 @@ public class AddOrUpdatePromotionCommandHandler : IAddOrUpdatePromotionCommandHa
 
         var promotion = Promotion.Create(command.Description, command.Period, command.DiscountRule);
 
+        promotion.CheckVigency(DateTime.UtcNow);
+
+        foreach (var gamePublicId in command.GamePublicIds.Distinct())
+        {
+            var game = await _gameQueryRepository.GetByIdAsync(gamePublicId, cancellationToken);
+            if (game is not null)
+            {
+                promotion.Games.Add(game);
+            }
+        }
+
+        foreach (var userPublicId in command.UserPublicIds.Distinct())
+        {
+            var user = await _userQueryRepository.GetByIdAsync(userPublicId, cancellationToken);
+            if (user is not null)
+            {
+                promotion.Users.Add(user);
+            }
+        }
 
         if (promotionExists)
             await _promotionCommandRepository.UpdateAsync(promotion, cancellationToken);
